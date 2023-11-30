dc2014781e706e0fe9f09082c7460bb0083199aa
wantSome <kihyun1998@naver.com>
2023-11-30 16:38:03 +0900

    text field 스타일링

--- lib/utils/app_styles.dart
+++ lib/utils/app_styles.dart
@@ -5,7 +5,12 @@ class AppTheme {
   static const Color medium = Color(0x50FFFFFF); // dark & light 모두 사용가능한 중간 흰색
   static const Color splash = Color(0xFF9b59b6);
   static const Color accent = Color.fromARGB(255, 150, 68, 182);
+  static const Color light = Color(0xFFFFFFFF);
 
   static const Color disabledBackgroundColor = Colors.black12;
   static const Color disabledForegroundColor = Colors.white12;
+
+  static const TextStyle textStyle20 = TextStyle(color: light, fontSize: 15);
+  static const TextStyle hintStyle = TextStyle(color: medium);
+  static const TextStyle counterStyle = TextStyle(color: medium, fontSize: 10);
 }
--- lib/widgets/custom_textfield.dart
+++ lib/widgets/custom_textfield.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meta_note/utils/app_styles.dart';
 
 class CustomTextField extends StatefulWidget {
   final int maxLength;
@@ -19,8 +20,40 @@ class CustomTextField extends StatefulWidget {
 }
 
 class _CustomTextFieldState extends State<CustomTextField> {
+  final _focusNode = FocusNode();
+
+  // free 해주는거랑 비슷한 느낌인가?
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return TextField(
+      focusNode: _focusNode,
+      onEditingComplete: () => FocusScope.of(context).nextFocus(),
+      controller: widget.controller, // widget을 사용하면 위젯 정보 가져올 수 있다.
+      maxLength: widget.maxLength,
+      maxLines: widget.maxLines,
+      keyboardType: TextInputType.multiline,
+      cursorColor: AppTheme.accent,
+      style: AppTheme.textStyle20,
+      decoration: InputDecoration(
+          hintStyle: AppTheme.hintStyle,
+          hintText: widget.hintText,
+          focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide(
+              color: AppTheme.accent,
+            ),
+          ),
+          enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(
+              color: AppTheme.medium,
+            ),
+          ),
+          counterStyle: AppTheme.counterStyle),
+    );
   }
 }
